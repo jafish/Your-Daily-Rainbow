b22a5db8ec90b09c7594f874846a38a034077b4a
Josh Fishburn <josh@joshfishburn.com>
2017-04-15 19:04:17 -0400

    Added Rotation and Taskpaper document

--- Rainbows-P5-Webcam/sketch.js
+++ Rainbows-P5-Webcam/sketch.js
@@ -4,26 +4,43 @@ var f, f1;
 var pos = 0.00003;
 var capture;
 var button;
+var fIncrement = 85;
+var f1Increment = PI/16;
 
 function setup() {
  createCanvas(windowWidth, windowHeight);
  capture = createCapture(VIDEO);
  capture.hide();
  imageMode(CENTER);
- button = createButton('hide sketch');
- button.position(10, 44);
- button.mousePressed(toggleSketch);
+ 
+ //button = createButton('hide sketch');
+ //button.position(10, 44);
+ //button.mousePressed(toggleSketch);
 }
 
 function draw() {
- // background(255);
- translate(width / 2, height / 2);
- for (f = 0; f < windowWidth; f += 85) {
-  for (f1 = 0; f1 < windowHeight; f1 += 85) {
-   rotate(PI / pos);
-   image(capture, f + random(1, 10), f1 + random(1, 10), 65, 65, f + random(0.5, 1), f1, [80 + random(1, 3)], [80]);
+  
+ background(255);
+ //translate(100, 100);
+ translate(width / 4, height / 4);
+ //image(capture, 0, 0, 320, 240, 50, 50, 320, 240);
+ //translate(100, 0);
+ //image(capture, 0, 0, 640, 240, 0, 0);
+ 
+ //fIncrement += 0.5;
+ 
+ 
+ console.log(fIncrement + ", " + f1Increment);
+ for (f = 0; f < windowWidth; f += fIncrement) {
+  for (f1 = 0; f1 < windowHeight; f1 += fIncrement) {
+    //image(capture, f, f1, 65, 85);
+    rotate(f1Increment);
+    //image(capture, f + random(1, 10), f1 + random(1, 10), 65, 65, f + random(0.5, 1), f1, [80 + random(1, 3)], [80]);
+    image(capture, f, f1, 65, 65, f, f1, fIncrement-5, fIncrement-5);
   }
  }
+ //fIncrement = mouseX;
+ f1Increment = PI*mouseY/1028;
 }
 
 function mouseWheel(event) {
