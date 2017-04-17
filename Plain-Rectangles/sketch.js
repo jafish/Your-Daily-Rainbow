f7cac11cbe0a4faee2ebefec45193f896604cef9
Josh Fishburn <josh@joshfishburn.com>
2017-04-16 21:18:46 -0400

    Added alpha adjustment and animation reset
    - Spacebar to reset animation
    - Up and down arrows to increase/decrease alpha, respectively

--- Plain-Rectangles/sketch.js
+++ Plain-Rectangles/sketch.js
@@ -20,32 +20,31 @@ function setup() {
   imageMode(CENTER);
   rWidth = 0;
   noStroke();
+}
+
+function draw() {
+  background(0, 0, 255);
   
+  // Set the color variables here in draw() so that alpha can be adjusted
   rC = color(253, 160, 148, a);
   oC = color(253, 204, 147, a);
   yC = color(254, 235, 152, a);
   gC = color(162, 244, 192, a);
   bC = color(145, 216, 253, a);
   pC = color(203, 176, 253, a);
-}
-
-function draw() {
-  background(0, 0, 255);
-  //translate(width / 2, height / 2);
-  //console.log("Capture width is " + capture.width + ", and height is " + capture.height);
-
-  image(capture, width/2, height/2);
   
-  rX = width/2 - capture.width/2;
-  rY = height/2 - capture.height/2;
-  rYIncrement = capture.height/6;
-  
+  image(capture, width / 2, height / 2);
+
+  rX = width / 2 - capture.width / 2;
+  rY = height / 2 - capture.height / 2;
+  rYIncrement = capture.height / 6;
+
   target = capture.width;
   rWidth += (target - rWidth) * easing;
-  
+
   fill(rC); // Red
   rect(rX, rY, rWidth, rYIncrement);
-  
+
   fill(oC); // Orange
   rect(rX, rY + rYIncrement * 1, rWidth, rYIncrement);
 
@@ -60,4 +59,32 @@ function draw() {
 
   fill(pC); // Purple
   rect(rX, rY + rYIncrement * 5, rWidth, rYIncrement);
+
+  // Change alpha value
+  if (keyIsPressed) {
+    if (keyCode === UP_ARROW) {
+      a++;
+      if (a >= 255) {
+        a = 255;
+      }
+    } else if (keyCode === DOWN_ARROW) {
+      a--;
+      if (a <= 0) {
+        a = 0;
+      }
+    }
+  }
+}
+
+function keyPressed() {
+  if (key === " ") {
+    // Reset the rainbow to the left of the capture
+    rWidth = 0;
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  // Make the image display size proportional to the window
+
 }
