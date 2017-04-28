dc9786cb03a5910f180bc95397223afec99de6df
Josh Fishburn <josh@joshfishburn.com>
2017-04-28 00:56:15 -0400

    Swapped mouse/keyboard; added analytics

--- Plain-Rectangles/sketch.js
+++ Plain-Rectangles/sketch.js
@@ -23,7 +23,7 @@ function setup() {
   rWidth = 0;
   noStroke();
   textFont("Lobster");
-  
+
   // Setup the initial colors and color array
   rC = color(253, 160, 148, a);
   oC = color(253, 204, 147, a);
@@ -32,43 +32,39 @@ function setup() {
   bC = color(145, 216, 253, a);
   pC = color(203, 176, 253, a);
   colors = [rC, oC, yC, gC, bC, pC];
-  
+
   // Get a random color with which to set the background each time
   var rnum = Math.floor(random(6));
   backgroundColor = colors[rnum];
   rnum += 2;
   rnum = rnum % 6;
-  
+
   textColor = colors[rnum];
 }
 
 function draw() {
   background(backgroundColor);
-  
-  var nonCaptureVSpace = (height-capture.height)/2;
-  var nonCaptureHSpace = (width-capture.width)/2;
-  
+
+  var nonCaptureVSpace = (height - capture.height) / 2;
+  var nonCaptureHSpace = (width - capture.width) / 2;
+
   fill(textColor);
   textSize(36);
   textAlign(CENTER);
-  text("Your Daily Rainbow", width/2, nonCaptureVSpace - height/40);
-  
-  if (keyIsPressed) {
-  
-  }
-  if ()
+  text("Your Daily Rainbow", width / 2, nonCaptureVSpace - height / 40);
+
   fill(textColor);
   textSize(16);
   textAlign(CENTER);
-  text("Come back tomorrow for another filter...", width/2, height - nonCaptureVSpace + height/40);
-  
+  text("Come back tomorrow for another filter...", width / 2, height - nonCaptureVSpace + height / 40);
+
   fill(textColor);
   textSize(16);
   textAlign(LEFT);
   text("⬍: Adjust Transparency", width - nonCaptureHSpace + 4, height - nonCaptureVSpace - 36);
   text("Mouse Click: Reset Filter Animation", width - nonCaptureHSpace + 4, height - nonCaptureVSpace - 20);
   text("Spacebar: Save PNG Snapshot", width - nonCaptureHSpace + 4, height - nonCaptureVSpace - 4);
-  
+
   // Set the color variables here in draw() so that alpha can be adjusted
   rC = color(253, 160, 148, a);
   oC = color(253, 204, 147, a);
@@ -76,7 +72,7 @@ function draw() {
   gC = color(162, 244, 192, a);
   bC = color(145, 216, 253, a);
   pC = color(203, 176, 253, a);
-  
+
   image(capture, width / 2, height / 2);
 
   rX = width / 2 - capture.width / 2;
@@ -121,13 +117,10 @@ function draw() {
 }
 
 function keyPressed() {
-  if (key === " ") {
-    // Reset the rainbow to the left of the capture
-    rWidth = 0;
-  } else if (key === "a" || key === "A") {
+  if (key === "a" || key === "A") {
     // Try to dynamically create an image based on the fact that the image
     // is in the center and based on the size of the video feed
-    
+
     //capture.save("rainbow", "png");
     //capture.loadPixels();
     // for (i = 0; i < capture.width; i++) {
@@ -140,11 +133,18 @@ function keyPressed() {
   }
 }
 
+function keyReleased() {
+  if (key === " ") {
+    saveCanvas(capture, 'rainbow', 'png');
+  }
+}
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   // Make the image display size proportional to the window
 }
 
 function mousePressed() {
-  saveCanvas(capture, 'rainbow', 'png');
+  // Reset the rainbow to the left of the capture
+  rWidth = 0;
 }
